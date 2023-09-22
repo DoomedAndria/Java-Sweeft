2a006a0b522e32210322c0bc38c5a023a553440b
KIU-PC <bibiashviliandria@gmail.com>
2023-09-22 19:08:51 +0400

    fix

--- src/Ex4.java
+++ src/Ex4.java
@@ -1,12 +1,10 @@
 public class Ex4 {
     public static void main(String[] args) {
-        System.out.print(addBinStrings("11","10"));
+        System.out.print(addBinStrings("0","101"));
     }
 
     static String addBinStrings(String n1, String n2) {
-        int len1 = n1.length();
-        int len2 = n2.length();
-        if (len2 > len1) {
+        if (n2.length() > n1.length()) {
             String temp = n1;
             n1 = n2;
             n2 = temp;
@@ -14,14 +12,14 @@ public class Ex4 {
         String res = "";
         int carry = 0;
         int sum;
-        int i2= len2-1;
-        for (int i = len1-1; i >=0 ; i--) {
+        int i2= n2.length()-1;
+        for (int i = n1.length()-1; i >=0 ; i--) {
             if(i2>=0) {
                 sum = (n1.charAt(i) + n2.charAt(i2) - 2 * (int) '0') + carry;
                 i2--;
             }
             else
-                sum = (n1.charAt(i)-2*(int)'0') + carry;
+                sum = (n1.charAt(i)-(int)'0') + carry;
             carry = sum/2;
             res = sum%2 + res;
         }
